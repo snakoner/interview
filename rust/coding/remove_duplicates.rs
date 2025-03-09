f85a0949aad91a7f88b4d34db5db49600b4a2e33
snakoner <andrstroganov@yandex.ru>
2025-03-09 10:59:56 +0300

    Create remove_duplicates.rs

---
+++ rust/coding/remove_duplicates.rs
@@ -0,0 +1,18 @@
+fn remove_duplicates(vec: &mut Vec<i32>) -> usize {
+    if vec.len() == 0 {
+        return 0;
+    }
+
+    let mut prev = vec[0];
+    let mut pos: usize = 1;
+
+    for i in 1..vec.len() {
+        if prev != vec[i] {
+            vec[pos] = vec[i];
+            pos += 1;
+            prev = vec[i];
+        }
+    }
+
+    pos
+}
