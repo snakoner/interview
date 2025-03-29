cdba806e7c664d2b7a8787bf6a9b7ca091f78f09
snakoner <andrstroganov@yandex.ru>
2025-03-29 16:28:30 +0300

    Update MultisigWallet.sol

--- solidity/contracts/MultisigWallet.sol
+++ solidity/contracts/MultisigWallet.sol
@@ -1,162 +1,213 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.28;
 
-contract MultisigWallet {
-    event Deposit(address indexed sender, uint amount, uint balance);
-    event SubmitTransaction(
-        address indexed owner, 
-        uint indexed index, 
-        address indexed to, 
-        uint value, 
-        bytes data
-    );
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-    event ConfirmTransaction(address indexed owner, uint index);
-    event RevokeConfirmation(address indexed owner, uint index);
-    event ExecuteTransaction(address indexed owner, uint index);
+interface IWallet {
+    error InvalidConstructorData(address failedAccount);
+    error RoleAlreadySet();
+    error RoleNotSet();
+    error UnauthorizedAccess();
+    error UnexpectedTransactionState(bytes32 expectedState);
+    error InsufficientEthSent();
+    error AlreadyConfirmed();
+    error NotEnoughConfirmations();
+    error TransactionFailed();
+    error InvalidConfirmatorsNumber();
 
+    event ExecutorAdded(address indexed account);
+    event ConfirmatorAdded(address indexed account);
+    event ConfirmatorRemoved(address indexed account);
+    event ProposerAdded(address indexed account);
+    event TransactionScheduled(bytes32 indexed _hash);
+    event TransactionConfirmed(bytes32 indexed _hash);
+    event TransactionExecuted(bytes32 indexed _hash);
+    event TransactionClosed(bytes32 indexed _hash);
+}
+
+contract Wallet is Ownable, IWallet {
+    enum TransactionState {
+        NonExist,
+        Created,
+        Closed,
+        Executed
+    }
 
     struct Transaction {
-        address to;
-        bool executed;
-        uint confirmations;
-        uint value;
+        uint256 value;        
+        uint256 nonce;
         bytes data;
+        address to;
+        TransactionState state;
+        uint64 confirmations;
     }
 
-    address[] public owners;
-    uint public numOfConfirmations;
+    mapping (bytes32 _hash => Transaction) private _transactions;
+    mapping (bytes32 _hash => mapping (address => bool)) private _confirmed;
+    mapping (address => bool) private _confirmators;
+    mapping (address => bool) private _executors;
+    mapping (address => bool) private _proposers;
+    uint64 private numConfirmators;
+    uint64 public immutable MIN_CONFIRMATIONS;
 
-    uint private numberExecuted;
-    mapping (address => bool) private isOwner;
-    mapping (uint => mapping (address => bool)) private confirmed;
-    Transaction[] private txs;
+    modifier onlyProposer(address account) {
+        require(_proposers[account], UnauthorizedAccess());
+        _;
+    }
 
-    constructor(address[] memory _owners, uint _numOfConfirmations) {
-        require(_owners.length > 0 && _numOfConfirmations > 0, "invalid input values");
-        require(_numOfConfirmations <= _owners.length, "invalid number of confirmations");
+    modifier onlyConfirmator(address account) {
+        require(_confirmators[account], UnauthorizedAccess());
+        _;
+    }
 
-        uint _length = _owners.length;
+    modifier onlyExecutor(address account) {
+        require(_executors[account], UnauthorizedAccess());
+        _;
+    }
 
-        for (uint i = 0; i < _length; i++) {
-            address newOwner = _owners[i];
-            require(newOwner != address(0), "owner cant be zero");
-            require(!isOwner[newOwner], "not unique owner");
-            owners.push(newOwner);
+    constructor(
+        uint64 _minConfirmations,
+        address[] memory confirmators,
+        address[] memory executors,
+        address[] memory proposers
+    ) Ownable(_msgSender()) {
+        uint256 confirmatorsLength = confirmators.length;
+        require(_minConfirmations <= confirmatorsLength);
 
-            isOwner[newOwner] = true;
+        numConfirmators = uint64(confirmatorsLength);
+        for (uint256 i = 0; i < confirmatorsLength; i++) {
+            require(!_confirmators[confirmators[i]], InvalidConstructorData(confirmators[i]));
+            _confirmators[confirmators[i]] = true;
+
+            emit ExecutorAdded(confirmators[i]);
         }
+
+        for (uint256 i = 0; i < executors.length; i++) {
+            require(!_executors[executors[i]], InvalidConstructorData(executors[i]));
+            _executors[executors[i]] = true;
+
+            emit ExecutorAdded(executors[i]);
+        }
+
+        for (uint256 i = 0; i < proposers.length; i++) {
+            require(!_proposers[proposers[i]], InvalidConstructorData(proposers[i]));
+            _proposers[proposers[i]] = true;
+
+            emit ProposerAdded(proposers[i]);
+        }
+
+        MIN_CONFIRMATIONS = _minConfirmations;
     }
 
-    modifier onlyOwner() {
-        require(isOwner[msg.sender], "not an owner");
-        _;
-    }
-
-    modifier notConfirmed(uint index) {
-        require(confirmed[index][msg.sender] == false, "already confirmed");
-        _;
-    }
-
-    modifier notExectuted(uint index) {
-        require(txs[index].executed == false, "already executed");
-        _;
-    }
-
-    modifier txExists(uint index) {
-        require(index < txs.length, "tx not exists");
-        _;
-    }
-
-    receive() external payable {
-        emit Deposit(msg.sender, msg.value, address(this).balance);
-    }
-
-    function submitTransaction(
-        address _to,
-        uint _value,
-        bytes calldata _data
-    ) external onlyOwner {
-        uint index = txs.length;
-
-        txs.push(Transaction({
-            to: _to,
-            executed: false,
-            confirmations: 0,
-            value: _value,
-            data: _data
-        }));
-
-        emit SubmitTransaction(msg.sender, index, _to, _value, _data);
-    }
-
-    function confirmTransaction(uint index) external 
-        onlyOwner 
-        txExists(index) 
-        notConfirmed(index)
-        notExectuted(index)
-    {
-        confirmed[index][msg.sender] = true;
-        txs[index].confirmations++;
-
-        emit ConfirmTransaction(msg.sender, index);
-    }
-
-    function revokeConfirmation(uint index) external 
-        onlyOwner
-        txExists(index)
-        notExectuted(index) 
-    {
-        require(confirmed[index][msg.sender], "not confirmed");
-        confirmed[index][msg.sender] = true;
-        txs[index].confirmations--;
-
-        emit RevokeConfirmation(msg.sender, index);
-    }
-
-    function executeTransaction(uint index) external 
-        onlyOwner 
-        txExists(index)
-        notExectuted(index)
-    {
-        require(txs[index].confirmations >= numOfConfirmations, "no enough confirmations");
-
-        Transaction storage _tx = txs[index];
-        _tx.executed = true;
-
-        (bool success, ) = payable(_tx.to).call{value: _tx.value}(_tx.data);
-        require(success, "tx failed");
-
-        numberExecuted++;
-
-        emit ExecuteTransaction(msg.sender, index);
-    }
-
-    function getOwners() external view returns (address[] memory) {
-        return owners;
-    }
-
-    function getTransactionCount() external view returns (uint) {
-        return txs.length;
-    }
-
-    function getNumOfExecutedTransactions() external view returns (uint) {
-        return numberExecuted;
-    }
-
-    function getTransaction(uint index) external view txExists(index) returns (
+    function schedule(
+        uint256 value,
         address to,
-        uint value,
-        bytes memory data,
-        bool executed,
-        uint confirmations
-    ) {
-        return (
-            txs[index].to,
-            txs[index].value,
-            txs[index].data,
-            txs[index].executed,
-            txs[index].confirmations
+        bytes calldata data,
+        uint256 nonce
+    ) external payable onlyProposer(msg.sender) {
+        bytes32 _hash = hashify(value, nonce, to, data);
+        require(
+            _transactions[_hash].state == TransactionState.NonExist, 
+            UnexpectedTransactionState(_decodeState(TransactionState.NonExist))
         );
+
+        require(msg.value >= value, InsufficientEthSent());
+
+        _transactions[_hash] = Transaction({
+            value: msg.value,
+            nonce: nonce,
+            data: data,
+            to: to,
+            state: TransactionState.Created,
+            confirmations: 0            
+        });
+
+        emit TransactionScheduled(_hash);
+    }
+
+    function confirm(bytes32 _hash) external onlyConfirmator(msg.sender) {
+        TransactionState state = _transactions[_hash].state;
+        require(
+            state == TransactionState.Created, 
+            UnexpectedTransactionState(_decodeState(TransactionState.Created))
+        );
+
+        require(!_confirmed[_hash][msg.sender], AlreadyConfirmed());
+        _transactions[_hash].confirmations += 1;
+        _confirmed[_hash][msg.sender] = true;
+
+        emit TransactionConfirmed(_hash);
+    }
+
+    function execute(bytes32 _hash) external onlyExecutor(msg.sender) {
+        Transaction storage _tx = _transactions[_hash];
+        TransactionState state = _tx.state;
+        require(
+            state == TransactionState.Created, 
+            UnexpectedTransactionState(_decodeState(TransactionState.Created))
+        );
+
+        require(_tx.confirmations >= MIN_CONFIRMATIONS, NotEnoughConfirmations());
+        _tx.state = TransactionState.Executed;
+        (bool success, ) = _tx.to.call{value: _tx.value}(_tx.data);
+        require(success, TransactionFailed());
+
+        emit TransactionExecuted(_hash);
+    }
+
+    function close(bytes32 _hash) external onlyProposer(msg.sender) {
+        TransactionState state = _transactions[_hash].state;
+        require(
+            state == TransactionState.Created, 
+            UnexpectedTransactionState(_decodeState(TransactionState.Created))
+        );
+
+        _transactions[_hash].state = TransactionState.Closed;
+
+        emit TransactionClosed(_hash);
+    }
+
+    function _decodeState(TransactionState state) internal pure returns (bytes32) {
+        return bytes32(1 << uint8(state));
+    }
+
+    function addConfirmator(address confirmator) external onlyOwner {
+        require(!_confirmators[confirmator], RoleAlreadySet());
+
+        _confirmators[confirmator] = true;
+        numConfirmators++;
+        emit ConfirmatorAdded(confirmator);
+    }
+
+    function removeConfirmator(address confirmator) external onlyOwner {
+        require(numConfirmators - 1 >= MIN_CONFIRMATIONS, InvalidConfirmatorsNumber());
+        require(_confirmators[confirmator], RoleNotSet());
+
+        _confirmators[confirmator] = false;
+        numConfirmators--;
+        emit ConfirmatorRemoved(confirmator);
+    }
+
+    function addProposer(address proposer) external onlyOwner {
+        require(!_proposers[proposer], RoleAlreadySet());
+
+        _proposers[proposer] = true;
+        emit ProposerAdded(proposer);
+    }
+
+    function removeProposer(address proposer) external onlyOwner {
+        require(_proposers[proposer], RoleNotSet());
+
+        _proposers[proposer] = false;
+        emit ProposerAdded(proposer);
+    }
+
+    function hashify(
+        uint256 value,
+        uint256 nonce,
+        address to,
+        bytes calldata data
+    ) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(value, to, data, nonce));
     }
 }
