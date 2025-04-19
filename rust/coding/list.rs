694d3a038d834a2c426d5da051d63ea38a5a374a
snakoner <andrstroganov@yandex.ru>
2025-04-19 15:40:18 +0300

    Create list.rs

---
+++ rust/coding/list.rs
@@ -0,0 +1,58 @@
+
+#[derive(Debug)]
+struct ListNode {
+    next: Option<Box<ListNode>>,
+    value: i32
+}
+
+impl ListNode {
+    fn new(value: i32) -> Self {
+        Self { next: None, value: value }
+    }
+}
+
+#[derive(Debug)]
+struct List {
+    head: Option<Box<ListNode>>,
+    size: usize
+}
+
+impl List {
+    fn new() -> Self {
+        Self { head: None, size: 0 }
+    }
+
+    fn len(&self) -> usize {
+        self.size
+    }
+
+    fn push(&mut self, value: i32) {
+        self.size += 1;
+        let new_node = Box::new(ListNode::new(value));
+        if let None = self.head {
+            self.head = Some(new_node);
+            return;
+        }
+
+        let mut current = &mut self.head;
+        while let Some(ref mut node) = *current {
+            if node.next.is_none() {
+                node.next = Some(new_node);
+                return;
+            }
+            current = &mut node.next; // переходим к следующему узлу
+        }
+    }
+
+    fn println(&mut self) {
+        let mut current = &mut self.head;
+        while let Some(ref mut node) = current {
+            print!("{} ", node.value);
+            current = &mut node.next;
+        }
+    }
+
+    fn pop(&mut self) {
+
+    }
+}
