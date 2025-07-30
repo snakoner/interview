a5a2624adf1ea01248fd620eb9d6eaf152e5d5c0
snakoner <andrstroganov@yandex.ru>
2025-07-30 17:22:44 +0300

    Update compress-string.rs

--- rust/coding/compress-string.rs
+++ rust/coding/compress-string.rs
@@ -28,9 +28,55 @@ fn compress_string(s: &str) -> String {
     res
 }
 
+fn decompress_string(s: &str) -> String {
+    let mut result = String::new();
+    if s.len() <= 1 {
+        return String::from(s);
+    }
+
+    let mut prev = s.chars().next().unwrap();
+    for ch in s.chars() {
+        if ch.is_alphabetic() {
+            result.push(ch);
+            prev = ch;
+        } else {
+            let value = ch.to_digit(10).unwrap();
+            for _ in 0..value - 1 {
+                result.push(prev);
+            }
+        }                     
+    }
+
+    result
+}
+
 fn main() {
     assert_eq!(compress_string("heeellllooo"), "he3l4o3");
     assert_eq!(compress_string("h"), "h");
     assert_eq!(compress_string("hhha"), "h3a");
     assert_eq!(compress_string("heeellllo"), "he3l4o");
+    assert_eq!(compress_string(""), "");
+
+    {
+        let mut s = "heeellllooo";
+        let decompressed_string = decompress_string(&compress_string(s));
+        assert_eq!(s, decompressed_string);
+    }
+
+    {
+        let mut s = "hhha";
+        let decompressed_string = decompress_string(&compress_string(s));
+        assert_eq!(s, decompressed_string);
+    }
+
+    {
+        let mut s = "h";
+        let decompressed_string = decompress_string(&compress_string(s));
+        assert_eq!(s, decompressed_string);
+    }
+    {
+        let mut s = "heeellllo";
+        let decompressed_string = decompress_string(&compress_string(s));
+        assert_eq!(s, decompressed_string);
+    }
 }
